d86ca080b1d4de15494f4a7084ef133b81a193eb
RayRoad <rayroad@126.com>
2016-04-13 12:44:46 +0800

    Site updated: 2016-04-13 12:44:44

--- odometer/js/index2.js
+++ odometer/js/index2.js
@@ -2,6 +2,9 @@ var odometerMiles = 21000573;
 window.odometerPositions = new Array();
 var odometerMilesStr = odometerMiles.toString();
 var $window = $(window);
+var startTime = 0;
+var endTime = 0;
+var easing = 'linear';
 var odometerPlaces = [
   'ones',
   'tens',
@@ -15,58 +18,83 @@ var odometerPlaces = [
   'billions',
   'fuck-ton'
 ];
-var odometerSpeed = 18000;
+var odometerSpeed = 60000;
 var animated = false;
+var animateNumsed = false;
+
 function positionNums() {
   var odometerNumHeight = $('.odometer-box').outerHeight();
   var odometerNumWidth = $('.odometer-box').outerWidth();
   var fontSize = (odometerNumWidth+40) >> 0;
   $(".odometer-nums li").css({"height":odometerNumHeight+"px","lineHeight":odometerNumHeight+"px","fontSize":fontSize+"px"})
-  singleColHeight = odometerNumHeight*10;
+  var singleColHeight = odometerNumHeight*10;
   for(var i = 0; i < odometerMilesStr.length; i++){
-    numOfNums = odometerMilesStr.length-i;
-    placeNum = odometerMilesStr.substr(numOfNums-1,1);
-    totalColHeight = $('#'+odometerPlaces[i]+' .odometer-nums').height();
-    reminderColHeight = singleColHeight-(placeNum*odometerNumHeight);
-    finalNumHeight = totalColHeight-reminderColHeight;
+    var numOfNums = odometerMilesStr.length-i;
+    var  placeNum = odometerMilesStr.substr(numOfNums-1,1);
+    var totalColHeight = $('#'+odometerPlaces[i]+' .odometer-nums').height();
+    var reminderColHeight = singleColHeight-(placeNum*odometerNumHeight);
+    var finalNumHeight = totalColHeight-reminderColHeight;
     window.odometerPositions[i] = finalNumHeight;
   }
 }
 
 function animateNums() {
+  if(animated){
+    easing = 'easeOutCirc'
+  }
   for(var i = 0; i < odometerMilesStr.length; i++){
     $('#'+odometerPlaces[i]+' .odometer-nums').stop().animate({
       top: -window.odometerPositions[i]+'px'
-    }, odometerSpeed, 'easeOutQuad',function () {
-      animated = true;
+    }, odometerSpeed, easing,function () {
+      animateNumsed = true;
     });
   }
   //odometerSpeed = 800;
 }
 
-jQuery(window).load(function() {
+
+function makeHtml() {
+  $('.odometer').empty();
   for(var i = 0; i < odometerMilesStr.length; i++){
     $('.odometer').prepend('<div id="'+odometerPlaces[i]+'" class="odometer-box"><ul class="odometer-nums"></ul></div>');
-    var numOfNums = odometerMilesStr.length-i;
-    for(var x = 0; x < numOfNums; x++){
-      for(var y = 0; y < 10; y++){
-        $('.odometer-nums').append('<li class="odometer-num-'+y+'">'+y+'</li>');
-      }
+  }
+  var numOfNums = Math.pow(odometerMilesStr.length,2);
+  for(var x = 0; x < numOfNums; x++){
+    for(var y = 0; y < 10; y++){
+      $('.odometer-nums').append('<li class="odometer-num-'+y+'">'+y+'</li>');
     }
   }
+}
+
+jQuery(window).load(function() {
+  makeHtml();
   positionNums();
 });
+
 $(document).keyup(function(e){
   var curKey = e.which;
   if(curKey==13){
-    animateNums();
+    if(!animated){
+      animateNums();
+      animated = true;
+
+    }else{
+      if(!animateNumsed){
+        odometerSpeed = 4000;
+        animateNums();
+        animated = false;
+      }
+    }
   }
 });
 
 $(window).resize(function() {
   positionNums();
   if(animated){
-    odometerSpeed = 800;
+    if(animateNumsed){
+      odometerSpeed = 800;
+    }
+    animateNums();
   }
-  animateNums();
+
 })
